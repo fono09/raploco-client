fcf8140d1fbe89dca0d7992f9a9ed6b1ef84ba2f
totetotetotem <kyarioka7945@gmail.com>
2017-09-09 17:32:28 +0900

    Initial Commit

---
+++ Assets/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SingletonMonoBehaviour<T>: MonoBehaviour where T : MonoBehaviour {
+    private static T mInstance;
+
+    public static T instance {
+        get {
+            if (SingletonMonoBehaviour<T>.mInstance == null) {
+                SingletonMonoBehaviour<T>.mInstance = (T)((object)UnityEngine.Object.FindObjectOfType(typeof(T)));
+            }
+            return SingletonMonoBehaviour<T>.mInstance;
+        }
+    }
+
+    public static bool IsEmpty() {
+        return SingletonMonoBehaviour<T>.mInstance == null;
+    }
+
+    virtual public void Awake()
+    {
+        if (SingletonMonoBehaviour<T>.mInstance == null) {
+            SingletonMonoBehaviour<T>.mInstance = (T)((object)UnityEngine.Object.FindObjectOfType (typeof(T)));
+        }
+        if (this != SingletonMonoBehaviour<T>.instance) {
+            Destroy(this);
+        }
+    }
+}
