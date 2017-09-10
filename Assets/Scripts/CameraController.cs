0b96febe1d7a25536427cbe77c4987f031d38336
“axis06” <withcomputer@gmail.com>
2017-09-11 02:41:19 +0900

    3D Mode

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class CameraController : MonoBehaviour {
     private GUIStyle labelStyle;
@@ -9,6 +10,29 @@ public class CameraController : MonoBehaviour {
     GameObject last_object;
 
     int ShowTimeCount;
+
+
+
+
+
+
+
+
+    public float speed = 0.001F;
+    private float startTime;
+    private float journeyLength;
+
+
+
+    GameObject Now_hand_fish;
+    
+    Vector3 First_Position;
+    
+    int move_hand_status;
+    
+
+
+
     void Start()
     {
         //this.labelStyle = new GUIStyle();
@@ -16,6 +40,7 @@ public class CameraController : MonoBehaviour {
         //this.labelStyle.normal.textColor = Color.white;
         //後述するがここで「Start」シーンのジャイロの値を取っている
 　　　　　start_gyro = StartCameraController.ini_gyro;
+        move_hand_status = 0;
 
     }
 
@@ -28,22 +53,66 @@ public class CameraController : MonoBehaviour {
             tn.SetActive(false);
         }
 		if (Physics.Raycast(ray,out hit, Mathf.Infinity)){
+            Debug.Log(hit.transform.name);
             if (hit.transform.name == "3DFishPoint(Clone)"){
                 hit.transform.Find("TaskName").gameObject.SetActive(true);
                 if(last_object ==  hit.transform.gameObject){
-                    ShowTimeCount +=1;
+                    ShowTimeCount +=1; 
                 }else{
                     ShowTimeCount = 0;
                 }
 
-                if(ShowTimeCount > 200){
-                    
+                if(ShowTimeCount > 50 && move_hand_status == 0){
+                    move_hand_status = 1;
+                    First_Position = hit.transform.position;
+                    Now_hand_fish = hit.transform.gameObject;
+                    startTime = Time.time;
+                    journeyLength = Vector3.Distance(hit.transform.position,this.transform.position);
+                }else{
+                    if(move_hand_status == 0){
+                        hit.transform.position = new Vector3 (hit.transform.position.x + (Mathf.Sin(ShowTimeCount)* 0.05f),hit.transform.position.y,hit.transform.position.z);
+                    }
                 }
-
                 last_object = hit.transform.gameObject;
+            }
+            else if (hit.transform.name == "back"){
+                if(move_hand_status == 2){
+                    move_hand_status = 0;
+                    Now_hand_fish.transform.parent = null;
+                    Now_hand_fish.transform.position = First_Position;
+                    Now_hand_fish.transform.GetComponent<Collider>().enabled = true;
+                }
+            }
+             else if (hit.transform.name == "del"){
+                if(move_hand_status == 2){
+                    move_hand_status = 0;
+                    Destroy(Now_hand_fish);
+                }
             }
         }
 
+
+
+        if (move_hand_status == 1){
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            Now_hand_fish.transform.position = Vector3.Lerp(Now_hand_fish.transform.position,this.transform.position, fracJourney);
+            float dis = Vector3.Distance(Now_hand_fish.transform.position,this.transform.position);
+            if (dis < 3.0f){
+                move_hand_status = 2;
+            }
+        }
+
+        if (move_hand_status == 2){
+            //Debug.Log(GetPositionOnSphere(5.0f,5.0f,1.0f));
+            //Now_hand_fish.transform.position = GetPositionOnSphere(Quaternion.AngleAxis(this.transform.rotation,Vector3.right),0.0f,1.0f);
+            
+            Now_hand_fish.transform.parent = this.transform;
+            //Now_hand_fish.transform.position = new Vector3(-3,0,0);
+            Now_hand_fish.transform.Find("TaskName").gameObject.SetActive(true);
+            Now_hand_fish.transform.GetComponent<Collider>().enabled = false;
+            //Now_hand_fish.transform.rotation = Quaternion.Euler(10,10,10);
+        }
 
 
         Input.gyro.enabled = true;
@@ -51,12 +120,21 @@ public class CameraController : MonoBehaviour {
         {
             gyro = Input.gyro.attitude;
             gyro = Quaternion.Euler(90, 0, 0) * (new Quaternion(-gyro.x,-gyro.y +start_gyro.y, gyro.z - start_gyro.z, gyro.w));
-            this.transform.localRotation = gyro;
+            //this.transform.localRotation = gyro;
             //最初に見ていた向きとゲームの進行方向を合わせる
             //this.transform.localRotation = Quaternion.Euler(0, -start_gyro.y, 0);
         }
 
     }
+
+    public Vector3 GetPositionOnSphere(float angle1, float angle2, float r)
+	{
+	        float x = r * Mathf.Sin(angle1) * Mathf.Cos(angle2);
+	        float y = r * Mathf.Sin(angle1) * Mathf.Sin(angle2);
+	        float z = r * Mathf.Cos(angle1);
+	        return new Vector3(x, y, z);
+	}
+
     //ジャイロセンサの値を表示するプログラム
     void OnGUI()
     {
@@ -91,4 +169,5 @@ public class CameraController : MonoBehaviour {
             }
         }
     }
+
 }
