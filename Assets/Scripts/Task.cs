6045da2959d6fadb28972b164e10d0c54392cbfb
totetotetotem <kyarioka7945@gmail.com>
2017-09-10 11:02:05 +0900

    add Task

---
+++ Assets/Scripts/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Task {
+    public int id;
+    public string name;
+    public int cost;
+    public string deadline;
+    public DateTime DeadlineTime {
+        get {
+            return Convert.ToDateTime (deadline);
+        }
+    }
+}
