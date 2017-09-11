120c92bc2820965b5aca10b41984b8d5d1c5bd94
“axis06” <withcomputer@gmail.com>
2017-09-11 09:39:45 +0900

    add fish

---
+++ Assets/Scripts/BearRotationController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearRotationController : MonoBehaviour {
+
+
+	public GameObject obj1;
+    private object ll;
+
+    void Start () {
+		obj1 = GameObject.Find("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.LookAt(obj1.transform);
+	}
+}
--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -57,7 +57,7 @@ public class MainController : MonoBehaviour {
 		TimeLabel.GetComponent<Text>().text = datetimeStrY+"/"+datetimeStrMo+"/"+datetimeStrD+"\n"+datetimeStrH+":"+String.Format("{0:D2}", datetimeStrM)+":"+String.Format("{0:D2}", datetimeStrS);
 	
         if (Screen.orientation == ScreenOrientation.Portrait) {
-                SceneManager.LoadScene ("Menu");
+                //SceneManager.LoadScene ("Menu");
         }
     }
 
